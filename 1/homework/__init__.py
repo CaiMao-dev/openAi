9f914cc2632f78d7fb450b97f03e5ef9918214af
maocai <P@ssw0rd>
2023-11-13 15:57:11 +0800

    fix 解决assistant没有记录问题

--- 1/homework/__init__.py
+++ 1/homework/__init__.py
@@ -9,22 +9,25 @@ import json
 def send(userId, msg):
     messages = ChartRecord.process_chat_message(userId, {"role": "user", "content": f"{msg}"})
     response = OpenAi.send_qa(messages);
-    return StreamingResponse(eventStream(response), media_type="text/event-stream")
+    return StreamingResponse(eventStream(userId, response), media_type="text/event-stream")
 
 # 以上代码获取chatgpt响应，
-def eventStream(response):
+def eventStream(userId, response):
+    msg = {"role": "assistant", "content": ""}
     for item in response:
         json_data = item['choices'][0]['delta']
         res_data = json.dumps(item['choices'][0]['delta'], ensure_ascii=False)
         if json_data.__contains__('content'):
+            msg["content"] = msg["content"] + json_data['content']
             yield 'data: {}\n\n'.format(res_data)
+    ChartRecord.process_chat_message(userId, msg)
 
 # 创建一个 FastAPI 应用
 app = FastAPI()
 
 @app.get("/analysed")
-async def analysed(str):
-    return send("caimao", str);
+async def analysed(msg):
+    return send("caimao", msg);
 
 uvicorn.run(app, host="0.0.0.0", port=18001)
 
